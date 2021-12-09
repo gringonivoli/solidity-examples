0ff7993dc8ca9a2d4d4979a21afce0ee02b31067
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2021-12-09 16:59:09 -0300

    feat: Add address msg example

---
+++ contracts/AddressMsg.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
+pragma solidity ^0.8.3;
+
+contract AddressMsg {
+    address public owner;
+
+	constructor() {
+		owner = msg.sender;
+	}
+}
